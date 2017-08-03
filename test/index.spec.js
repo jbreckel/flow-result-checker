975a3fc00ef63f23483e79d381a2598a574a86ab
Nathan Phillip Brink <ohnobinki@ohnopublishing.net>
2017-08-03 12:12:02 +0000

    Update usage test (#9)
    * Fix -h test and switch it to snapshots.
    
    To update snapshots in the future, use jest --updateSnapshot.
    
    * tests: Change space normalization to prettify spanshot.

--- test/index.spec.js
+++ test/index.spec.js
@@ -4,32 +4,17 @@ const createChecker = require('../checker')
 
 describe('main tool', () => {
   it('should display help', () => {
-    const expected = `
-      Usage: index [options]
-
-      Options:
-
-        -h, --help                   output usage information
-        -V, --version                output the version number
-        --no-color
-        --debugging
-        -d, --dir <dir>              The directory where to save the flow result and errors. If not specified no files will be saved.
-        -f, --file <filename>        Name of the result file. defaults to 'results.txt'
-        -e, --error-file <filename>  Name of the file where parsing errors are stored. defaults to 'error.txt'
-
-    `
-
     const { stdout } = spawnSync('node', ['index.js', '--help'], { encoding: 'utf8' })
 
-    expect(stdout.replace(/ /g, '')).toEqual(expected.replace(/ /g, ''))
+    expect(stdout.replace(/ +/g, ' ')).toMatchSnapshot();
   })
 
   it('should find no errors in `index.js`', () => {
     const expected = `Found 0 errors
-    `
+`
 
     const { stdout } = spawnSync('flow', ['check'], { encoding: 'utf8' })
 
-    expect(stdout.replace(/ /g, '')).toEqual(expected.replace(/ /g, ''))
+    expect(stdout.replace(/ +/g, ' ')).toEqual(expected.replace(/ +/g, ' '))
   })
 })
